dc595b1e7b78126ffd824c682301d262d485defb
Gabriel Olegario <91512651+BilOlegario@users.noreply.github.com>
2022-07-27 08:32:36 -0300

    Add files via upload

---
+++ src2/shop.js
@@ -0,0 +1,15 @@
+class Shop {
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  updateQuality() {
+    this.items.forEach((item) => {
+      item.updateQuality()
+    })
+    return this.items;
+  }
+}
+module.exports = {
+  Shop,
+}
